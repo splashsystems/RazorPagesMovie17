f3bf23380b1bb110b29526b8bf2968639454c5ec
Scot <bscot.hagen@gmail.com>
2021-06-16 14:49:02 -0700

    Added Validations, Data annotations

--- RazorPagesMovie17/Migrations/20210616214605_DataAnnotations.Designer.cs
+++ RazorPagesMovie17/Migrations/20210616214605_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie17.Data;
 
 namespace RazorPagesMovie17.Migrations
 {
     [DbContext(typeof(RazorPagesMovie17Context))]
-    partial class RazorPagesMovie17ContextModelSnapshot : ModelSnapshot
+    [Migration("20210616214605_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

53a23fcba86142532401a5aa52bb44cda2d9f340
Scot <bscot.hagen@gmail.com>
2021-06-16 14:28:09 -0700

    Added Movie model, Seed data.

---
+++ RazorPagesMovie17/Data/RazorPagesMovie17Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie17.Models;
+
+namespace RazorPagesMovie17.Data
+{
+    public class RazorPagesMovie17Context : DbContext
+    {
+        public RazorPagesMovie17Context (DbContextOptions<RazorPagesMovie17Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie17.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie17/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie17.Data;
+using RazorPagesMovie17.Models;
+
+namespace RazorPagesMovie17.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie17.Data.RazorPagesMovie17Context _context;
+
+        public IndexModel(RazorPagesMovie17.Data.RazorPagesMovie17Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
